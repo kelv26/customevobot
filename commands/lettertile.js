8a61e0da8a1098c062666f4948ae31be2406b1b1
Kelv26 <75353882+kelv26@users.noreply.github.com>
2021-12-27 14:24:27 +0800

    Fix Letter Tile and Word Snacks activity

--- commands/lettertile.js
+++ commands/lettertile.js
@@ -4,7 +4,7 @@ const client = new Client()//creating a new discord.js client
 
 module.exports = {
     name: "lettertile",
-    description: "Play Letter Tile in discord",
+    description: "Starts a Letter Tile session",
     usage: "",
     permissions: {
         channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
@@ -18,52 +18,18 @@ module.exports = {
      * @param {string[]} args
      * @param {*} param3
      */
-    run: async (client, message, args, { GuildDB }) => {
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to play something!**");
-        if(!message.member.voice.channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(message.channel, "❌ | **Bot doesn't have Create Invite Permission**");
-
+    async execute(message, args){
         let Invite = await message.member.voice.channel.activityInvite("879863686565621790")//Made using discordjs-activity package
         let embed = new MessageEmbed()
-        .setAuthor("Letter Tile", "https://media.discordapp.net/attachments/803959840547405854/907927062214488064/unknown.png?v=1")
+        .setAuthor("Letter Tile", "https://cdn.discordapp.com/emojis/749289646097432667.png?v=1")
         .setColor("#FF0000")
         .setDescription(`
-Using **Letter Tile** you can play Letter Tile with your friends in a Voice Channel. Click *Letter Tile* to join in!
-
-__**[Letter Tile](https://discord.com/invite/${Invite.code})**__
-
-⚠ **Note:** This only works in Desktop
-`)
-        message.channel.send(embed)
-    },
-    SlashCommand: {
-        options: [
-        ],
-    /**
-     *
-     * @param {import("../structures/DiscordMusicBot")} client
-     * @param {import("discord.js").Message} message
-     * @param {string[]} args
-     * @param {*} param3
-     */
-        run: async (client, interaction, args, { GuildDB }) => {
-            const guild = client.guilds.cache.get(interaction.guild_id);
-            const member = guild.members.cache.get(interaction.member.user.id);
-
-            if (!member.voice.channel) return client.sendTime(interaction, "❌ | You must be in a voice channel to use this command.");
-            if(!member.voice.channel.permissionsFor(guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(interaction, "❌ | **Bot doesn't have Create Invite Permission**");
-
-            let Invite = await member.voice.channel.activityInvite("879863686565621790")//Made using discordjs-activity package
-            let embed = new MessageEmbed()
-            .setAuthor("Letter Tile", "https://media.discordapp.net/attachments/803959840547405854/907927062214488064/unknown.png?v=1")
-            .setColor("#FF0000")
-            .setDescription(`
 Using **Letter Tile** you can play Letter Tile with your friends in a Voice Channel. Click *Join Letter Tile* to join in!
 
 __**[Join Letter Tile](https://discord.com/invite/${Invite.code})**__
 
 ⚠ **Note:** This only works in Desktop
 `)
-            interaction.send(embed.toJSON())
-        },
-    },
+        message.channel.send(embed)
+  }
 };
--- commands/wordsnacks.js
+++ commands/wordsnacks.js
@@ -4,7 +4,7 @@ const client = new Client()//creating a new discord.js client
 
 module.exports = {
     name: "wordsnacks",
-    description: "Play Word Snacks in discord",
+    description: "Starts a Words Snacks session",
     usage: "",
     permissions: {
         channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
@@ -18,52 +18,18 @@ module.exports = {
      * @param {string[]} args
      * @param {*} param3
      */
-    run: async (client, message, args, { GuildDB }) => {
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to play something!**");
-        if(!message.member.voice.channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(message.channel, "❌ | **Bot doesn't have Create Invite Permission**");
-
+    async execute(message, args){
         let Invite = await message.member.voice.channel.activityInvite("879863976006127627")//Made using discordjs-activity package
         let embed = new MessageEmbed()
-        .setAuthor("Word Snacks", "https://media.discordapp.net/attachments/803959840547405854/907967010183450694/unknown.png?v=1")
+        .setAuthor("YouTube Together", "https://cdn.discordapp.com/emojis/749289646097432667.png?v=1")
         .setColor("#FF0000")
         .setDescription(`
-Using **Word Snacks** you can play Word Snacks with your friends in a Voice Channel. Click *Word Snacks* to join in!
+Using **Words Snacks** you can play Wrods Snacks with your friends in a Voice Channel. Click *Join Words Snacks* to join in!
 
-__**[Word Snacks](https://discord.com/invite/${Invite.code})**__
+__**[Join Words Snacks](https://discord.com/invite/${Invite.code})**__
 
 ⚠ **Note:** This only works in Desktop
 `)
         message.channel.send(embed)
-    },
-    SlashCommand: {
-        options: [
-        ],
-    /**
-     *
-     * @param {import("../structures/DiscordMusicBot")} client
-     * @param {import("discord.js").Message} message
-     * @param {string[]} args
-     * @param {*} param3
-     */
-        run: async (client, interaction, args, { GuildDB }) => {
-            const guild = client.guilds.cache.get(interaction.guild_id);
-            const member = guild.members.cache.get(interaction.member.user.id);
-
-            if (!member.voice.channel) return client.sendTime(interaction, "❌ | You must be in a voice channel to use this command.");
-            if(!member.voice.channel.permissionsFor(guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(interaction, "❌ | **Bot doesn't have Create Invite Permission**");
-
-            let Invite = await member.voice.channel.activityInvite("879863976006127627")//Made using discordjs-activity package
-            let embed = new MessageEmbed()
-            .setAuthor("Word Snacks", "https://media.discordapp.net/attachments/803959840547405854/907967010183450694/unknown.png?v=1")
-            .setColor("#FF0000")
-            .setDescription(`
-Using **Word Snacks** you can play Word Snacks with your friends in a Voice Channel. Click *Join Word Snacks* to join in!
-
-__**[Join Word Snacks](https://discord.com/invite/${Invite.code})**__
-
-⚠ **Note:** This only works in Desktop
-`)
-            interaction.send(embed.toJSON())
-        },
-    },
+  }
 };
